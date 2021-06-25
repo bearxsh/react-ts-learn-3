a057ed54350915b66b4d18260d205802c5f8ae15
Bearxsh <bearxsh@163.com>
2021-06-25 14:47:16 +0800

    'update'

--- src/component/ShoppingList/ShoppingList.tsx
+++ src/component/ShoppingList/ShoppingList.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import instance from "../../axios-config";
+const {BrowserRouter, Switch, Route, Link}  = require("react-router-dom");
 
 function sayHello(name: string) {
     return <h1>{name}</h1>;
@@ -28,6 +30,18 @@ class ShoppingList extends React.Component<MyProps, MyState> {
     componentDidMount(): void {
         console.log("我被挂载了");
         this.timerId = window.setInterval(() => this.tick(), 1000);
+
+        instance.get("/")
+            .then(function (response: any) {
+                console.log(response.data);
+            })
+            .catch(function (error: any) {
+                console.log("出现错误！");
+                console.log(error);
+            })
+            .then(function () {
+                console.log("我总是被执行！")
+            })
     }
     componentWillUnmount(): void {
         console.log("我被卸载了");
@@ -45,23 +59,41 @@ class ShoppingList extends React.Component<MyProps, MyState> {
         }
         const numbers = [1,3,5,7,9];
         return (
-            <div className="shopping-list">
-                <h3>Shopping List for {sayHello("Jack")}</h3>
-                <ul>
-                    <li>{name}</li>
-                    <li>{name1}</li>
-                    <li>{this.props.name}</li>
-                    <li>{this.state.title}</li>
-                    <li>{this.state.date.toLocaleTimeString()}</li>
-                </ul>
-                <button onClick={this.handleClick.bind(this)}>{this.state.isToggleOn ? "ON" : "OFF"}</button>
-                <button onClick={this.deleteRow.bind(this, name)}>click me</button>
-                {button}
-                {isToggleOn && <div>开启</div>}
-                {isToggleOn ? <h1>开启</h1> : <h1>关闭</h1>}
-                {numbers.map((number) => <li key={number}>{number * 2}</li>)}
-                <input value={name}/>
-            </div>
+
+                <div className="shopping-list">
+                    <h3>Shopping List for {sayHello("Jack")}</h3>
+                    <ul>
+                        <li>{name}</li>
+                        <li>{name1}</li>
+                        <li>{this.props.name}</li>
+                        <li>{this.state.title}</li>
+                        <li>{this.state.date.toLocaleTimeString()}</li>
+                    </ul>
+                    <button onClick={this.handleClick.bind(this)}>{this.state.isToggleOn ? "ON" : "OFF"}</button>
+                    <button onClick={this.deleteRow.bind(this, name)}>click me</button>
+                    {button}
+                    {isToggleOn && <div>开启</div>}
+                    {isToggleOn ? <h1>开启</h1> : <h1>关闭</h1>}
+                    {numbers.map((number) => <li key={number}>{number * 2}</li>)}
+                    <input value={name}/>
+                    <BrowserRouter>
+                        <Link to="/about">About</Link>
+                        <Link to="/users">Users</Link>
+                        <Switch>
+                            <Route path="/about">
+                                <h1>我是about</h1>
+                            </Route>
+                            <Route path="/users">
+                                <h1>我是users</h1>
+                            </Route>
+                            <Route>
+                                <h1>page not found!</h1>
+                            </Route>
+                        </Switch>
+                    </BrowserRouter>
+                </div>
+
+
         );
     }
 
--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+{/*    <App />*/}
     <Footer />
     <ShoppingList name="Bearxsh" />
   </React.StrictMode>,
