1dd4c6a5718cc91a0bcf9fe80742133db1b06d47
Bearxsh <bearxsh@163.com>
2021-07-01 18:48:11 +0800

    update:

--- src/component/BearMenu/BearSubMenu.tsx
+++ src/component/BearMenu/BearSubMenu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import classNames from "classnames";
 import arrow from "./arrow.svg";
 
@@ -8,15 +8,21 @@ interface BearSubMenuProps {
 }
 
 function BearSubMenu(props: BearSubMenuProps) {
+    const subMenuRef = useRef<any>(null);
     const classes = classNames({
         "sub-menu": true
     });
+    const handleClick = () => {
+        if (subMenuRef) {
+            console.log(subMenuRef?.current?.clientHeight);
+        }
+    }
     return <div className={`sub-menu-wrap`}>
-        <div className={`sub-menu-wrap-title`}>
+        <div className={`sub-menu-wrap-title`} onClick={handleClick}>
             <span>{props.title}</span>
             <img src={arrow} className="arrow-icon" alt="arrow-icon"/>
         </div>
-        <div className={classes}>
+        <div className={classes} ref={subMenuRef}>
             <div className={`true-content`}>
                 {props.children}
             </div>
